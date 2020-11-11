44c35f4f3810bdbfd1eb8a778d4600758e81aa31
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-11-11 00:25:06 +0000

    updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 require("dotenv").config();
 const app = express();
 const cookieParser = require("cookie-parser");
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 3000;
 // Define express server and middleware
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  }));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/tastytreat",
@@ -25,15 +30,22 @@ mongoose.connect(
 const userRouter = require("./routes/User");
 app.use("/user", userRouter);
 
+app.use(cors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  }));
+  
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-
-  app.get("*", (request, response) => {
-    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 // Start the express server
 app.listen(PORT, () => {
   console.log("express server started");
